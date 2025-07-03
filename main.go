d3628ea793360e7126522bae399da2cfff2899a5
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-07-03 20:19:01 +0200

    Copied test

---
+++ main.go
@@ -0,0 +1,27 @@
+package querygen
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/Rick-Phoenix/querygen/db"
+	"github.com/Rick-Phoenix/querygen/db/sqlgen"
+	"github.com/labstack/gommon/log"
+)
+
+func TestMain(t *testing.T) {
+	database, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	querySchema := QueryGenSchema{
+		Name:    "GetUserWithPosts",
+		OutType: &db.PostWithUser{},
+		Queries: []QueryGroup{
+			{IsTx: true, Subqueries: []Subquery{{Method: "UpdatePost"}, {Method: "UpdateUser", NoReturn: true}}},
+			{Subqueries: []Subquery{{Method: "GetUser", SingleParamName: "userId"}}},
+		},
+		Store:      sqlgen.New(database),
+		OutputPath: "db/tttestquery.go",
+	}
+}
